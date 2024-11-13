177350d2b6def464e446a585ff56dc035ba080d8
EmotionlessDev <emotionlesscode@gmail.com>
2024-11-13 22:15:42 +1000

    opengl init

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,47 @@
-int main() {
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
 
+// callback function for window resizing
+void framebuffer_size_callback(GLFWwindow *pwindow, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+int main() {
+  // initialize GLFW
+  if (!glfwInit()) {
+    std::cout << "glfwInit failed" << std::endl;
+    return -1;
+  }
+  // set GLFW window hints
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // required for Mac
+#endif
+
+  // create a windowed mode window and its OpenGL context
+  GLFWwindow *pwindow = glfwCreateWindow(800, 600, "SandBox", nullptr, nullptr);
+  if (!pwindow) {
+    std::cout << "glfwCreateWindow failed" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(pwindow);
+
+  // initialize GLAD
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+  glfwSetFramebufferSizeCallback(pwindow, framebuffer_size_callback);
+
+  // render loop
+  while (!glfwWindowShouldClose(pwindow)) {
+    glfwSwapBuffers(pwindow);
+    glfwPollEvents();
+  }
+  glfwTerminate();
+  return 0;
 }
