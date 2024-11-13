093ce3be182d36fbfdbea44e601afc4d0790a057
EmotionlessDev <emotionlesscode@gmail.com>
2024-11-14 00:51:23 +1000

    triangles

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,64 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
 
 // callback function for window resizing
 void framebuffer_size_callback(GLFWwindow *pwindow, int width, int height) {
   glViewport(0, 0, width, height);
 }
+
+// quit the application when ESC is pressed
+void processInput(GLFWwindow *pwindow) {
+  if (glfwGetKey(pwindow, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(pwindow, true);
+  }
+}
+
+// load shader source code from file
+std::string loadShaderSource(const std::string &filename) {
+  std::ifstream file(filename);
+  if (!file.is_open()) {
+    std::cout << "Failed to open file: " << filename << std::endl;
+    return "";
+  }
+  std::stringstream buffer;
+  buffer << file.rdbuf();
+  return buffer.str();
+}
+
+float first_triangle[] = {
+    // first triangle
+    -1.0f, -0.5f, 0.0f, // bottom left
+    -0.5f, 0.5f,  0.0f, // top left
+    0.0f,  -0.5f, 0.0f, // bottom right
+};
+
+float second_triangle[] = {
+    // second triangle
+    0.0f, -0.5f, 0.0f, // bottom right
+    0.5f, 0.5f,  0.0f, // top right
+    1.0f, -0.5f, 0.0f  // bottom right
+};
+
+unsigned int indices[] = {
+    0, 1, 3, // первый треугольник
+    1, 2, 3  // второй треугольник
+};
+
+std::string vertexShaderSourceStr =
+    loadShaderSource("../shaders/vertex_shader.glsl");
+char const *vertexShaderSource = vertexShaderSourceStr.c_str();
+
+std::string fragmentShaderSourceStr =
+    loadShaderSource("../shaders/frag_shader.glsl");
+char const *fragmentShaderSource = fragmentShaderSourceStr.c_str();
+
+std::string fragmentShaderSourceYellowStr =
+    loadShaderSource("../shaders/frag_shader_yellow.glsl");
+char const *fragmentShaderSourceYellow = fragmentShaderSourceYellowStr.c_str();
 
 int main() {
   // initialize GLFW
@@ -37,8 +90,112 @@ int main() {
   }
   glfwSetFramebufferSizeCallback(pwindow, framebuffer_size_callback);
 
+  // init vertex shader
+  unsigned int vertexShader;
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+  glCompileShader(vertexShader);
+  // check for shader compile errors
+  int success;
+  char infoLog[512];
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+              << infoLog << std::endl;
+  }
+  // init fragment shader
+  unsigned int fragmentShader;
+  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+  glCompileShader(fragmentShader);
+  // check for shader compile errors
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+    std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
+              << infoLog << std::endl;
+  }
+  // init fragment shader yellow
+  unsigned int fragmentShaderYellow;
+  fragmentShaderYellow = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShaderYellow, 1, &fragmentShaderSourceYellow, nullptr);
+  glCompileShader(fragmentShaderYellow);
+  // check for shader compile errors
+  glGetShaderiv(fragmentShaderYellow, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(fragmentShaderYellow, 512, nullptr, infoLog);
+    std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
+              << infoLog << std::endl;
+  }
+  // link shaders into a shader program
+  unsigned int shaderProgram;
+  shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+  // check for linking errors
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+  if (!success) {
+    glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+    std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
+              << infoLog << std::endl;
+  }
+  // link shaders into a shader program yellow
+  unsigned int shaderProgramYellow;
+  shaderProgramYellow = glCreateProgram();
+  glAttachShader(shaderProgramYellow, vertexShader);
+  glAttachShader(shaderProgramYellow, fragmentShaderYellow);
+  glLinkProgram(shaderProgramYellow);
+  // check for linking errors
+  glGetProgramiv(shaderProgramYellow, GL_LINK_STATUS, &success);
+  if (!success) {
+    glGetProgramInfoLog(shaderProgramYellow, 512, nullptr, infoLog);
+    std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
+              << infoLog << std::endl;
+  }
+  // delete shaders
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+  // VAO1
+  unsigned int VAO1;
+  glGenVertexArrays(1, &VAO1);
+  glBindVertexArray(VAO1);
+
+  // VBO 1
+  unsigned int VBO1;
+  glGenBuffers(1, &VBO1);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO1);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(first_triangle), first_triangle,
+               GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+  glEnableVertexAttribArray(0);
+  // VAO2
+  unsigned int VAO2;
+  glGenVertexArrays(1, &VAO2);
+  glBindVertexArray(VAO2);
+  // VBO 2
+  unsigned int VBO2;
+  glGenBuffers(1, &VBO2);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(second_triangle), second_triangle,
+               GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+  glEnableVertexAttribArray(0);
+
   // render loop
   while (!glfwWindowShouldClose(pwindow)) {
+    processInput(pwindow);
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO1);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glUseProgram(shaderProgramYellow);
+    glBindVertexArray(VAO2);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
     glfwSwapBuffers(pwindow);
     glfwPollEvents();
   }
